732908ee28a9dfc3bcece9997eeb49816c723570
Pawan Pandit <thepawanpandiit@gmail.com>
2024-08-23 16:34:16 +0530

    add new folder

--- app/src/main/java/com/example/simpleapp/MainActivity.kt
+++ app/src/main/java/com/example/simpleapp/MainActivity.kt
@@ -34,7 +34,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!", fontSize = 30.sp,
+        text = "Hello $name! Welcome to modern World", fontSize = 30.sp,
         modifier = modifier
     )
 }
