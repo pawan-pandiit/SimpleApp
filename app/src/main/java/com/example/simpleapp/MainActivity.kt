58702a37cccde512d082d4ee12e19408b901e722
Pawan Pandit <thepawanpandiit@gmail.com>
2024-08-23 16:38:21 +0530

    syntax improved

--- app/src/main/java/com/example/simpleapp/MainActivity.kt
+++ app/src/main/java/com/example/simpleapp/MainActivity.kt
@@ -43,6 +43,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     SimpleAppTheme {
-        Greeting("Android")
+        Greeting("Android Developer")
     }
 }
